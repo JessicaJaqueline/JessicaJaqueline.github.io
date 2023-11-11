bfbbf2ea4017ad4706dbdaeb07a86c56d48e53b4
Jessica Jaqueline Ávila <31105483+JessicaJaqueline@users.noreply.github.com>
2023-11-11 11:50:35 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let typewriter = new Typewriter(app, {
  
 typewriter
   .pauseFor(2500)
-  .typeString('Egresada de la carrera Ingenierìa Industrial. ¡Estoy abierta a explorar posibilidades!')
+  .typeString('Egresada de la carrera Ingeniería Industrial. ¡Estoy abierta a explorar posibilidades!')
   .pauseFor(200)
   .deleteChars(10)
   .start();
